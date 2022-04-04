0f56a44a23a6f34e5c7b1065b02c1653c022697a
Sabine Detering <sabine.detering@web.de>
2022-04-04 14:53:59 +0200

    documentation world class

--- classes/world.class.js
+++ classes/world.class.js
@@ -15,6 +15,7 @@ class World {
     win_sound = new Audio('./audio/win.mp3');
     loose_sound = new Audio('./audio/loose.mp3');
 
+    animationInterval;
     drawRepeat;
 
     constructor(canvas, keyboard, level) {
@@ -31,8 +32,12 @@ class World {
         this.character.world = this;//allows character to use all variables of world
     }
 
+
+    /**
+     * starts animation functions for character, enemies and world
+     * animation for endboss is started when he shows up
+     */
     startAnimation() {
-        // console.log('animation started');
         this.character.animate();
         this.level.enemies.forEach(o => { o.animate(); });
         this.draw();
@@ -40,17 +45,24 @@ class World {
     }
 
 
+    /**
+     * stops all animation functions 
+     * enemies, collectable objects and bubbles are deleted
+     */
     stopAnimation() {
         this.character.stopAnimation();
         this.endboss.stopAnimation();
         this.level.enemies.forEach(o => { o.delete(this.level.enemies, o) });
-        this.level.collectableObjects.forEach(o => { o.delete(this.level.collectableObjects, o) });        
+        this.level.collectableObjects.forEach(o => { o.delete(this.level.collectableObjects, o) });
         this.bubbles.forEach(o => { o.delete(this.bubbles, o) });
         clearInterval(this.animationInterval);
         cancelAnimationFrame(this.drawRepeat);
     }
 
 
+    /**
+     * resets properties of objects that are present in all levels
+     */
     reset() {
         this.camera_x = 0;
         this.character.reset();
@@ -60,124 +72,130 @@ class World {
         this.coinBar.showStatus(0);
     }
 
+
+    /**
+     * main function for interactions between objects
+     */
     run() {
         this.animationInterval = setInterval(() => {
-                //character can only act if it is not dead or hurt
-                if (!this.character.isDead() && !this.character.isHurt()) {
+            //character can only act if it is not dead or hurt
+            if (!this.character.isDead() && !this.character.isHurt()) {
 
-                    this.character.calculateCollisionCoordinates();
+                this.character.calculateCollisionCoordinates();
 
-                    if (this.endboss.attackFinished && !this.character.killedByEndboss) {
-                        this.character.killedByEndboss = true;
-                        if (soundOn) { this.character.dead_sound.play(); }
-                        this.character.currentImage = 0;
-                        this.character.energy = 0;
-                        this.lifeBar.showStatus(0);
-                    }
+                if (this.endboss.attackFinished && !this.character.killedByEndboss) {
+                    this.character.killedByEndboss = true;
+                    if (soundOn) { this.character.dead_sound.play(); }
+                    this.character.currentImage = 0;
+                    this.character.energy = 0;
+                    this.lifeBar.showStatus(0);
+                }
 
-                    this.level.enemies.forEach((enemy) => {
-                        enemy.calculateCollisionCoordinates();
+                this.level.enemies.forEach((enemy) => {
+                    enemy.calculateCollisionCoordinates();
 
-                        if (enemy instanceof Pufferfish) {
+                    if (enemy instanceof Pufferfish) {
 
-                            if (this.character.isColliding(enemy) && !enemy.slappedNormal && !enemy.slappedInverse) {//slapped enemies don't hurt 
-                                // console.log('collision', enemy, this.character.energy);
-                                this.character.hit(enemy);
-                            } else if (this.keyboard.space && enemy.isSlapped(this.character)) {
-                                //slapping is only possible if character is not colliding
-                                // console.log('slapped');
-                                enemy.wait = true;
-                                if (this.character.otherDirection) {
-                                    enemy.slappedInverse = true;
-                                } else {
-                                    enemy.slappedNormal = true;
-                                }
-                            } else if (enemy.collisionMaxY < 0) {//slapped out of sight
-                                enemy.delete(this.level.enemies, enemy)
+                        if (this.character.isColliding(enemy) && !enemy.slappedNormal && !enemy.slappedInverse) {//slapped enemies don't hurt 
+                            // console.log('collision', enemy, this.character.energy);
+                            this.character.hit(enemy);
+                        } else if (this.keyboard.space && enemy.isSlapped(this.character)) {
+                            //slapping is only possible if character is not colliding
+                            // console.log('slapped');
+                            enemy.wait = true;
+                            if (this.character.otherDirection) {
+                                enemy.slappedInverse = true;
+                            } else {
+                                enemy.slappedNormal = true;
                             }
-                        } else {//Jellyfish
-                            if (this.character.isColliding(enemy)) {
-                                console.log('collision', enemy, this.character.energy);
-                                this.character.hit(enemy);
-                            }
+                        } else if (enemy.collisionMaxY < 0) {//slapped out of sight
+                            enemy.delete(this.level.enemies, enemy)
                         }
-                    });
-                }
+                    } else {//Jellyfish
+                        if (this.character.isColliding(enemy)) {
+                            console.log('collision', enemy, this.character.energy);
+                            this.character.hit(enemy);
+                        }
+                    }
+                });
+            }
 
-                // starting bubble attack
-                if (this.keyboard.b) {
-                    if (!this.character.isBubbling && !this.character.isBubblingPoison) {
-                        this.character.isBubbling = true;
-                        this.character.currentImage = 0;
-                    }
+            // starting bubble attack
+            if (this.keyboard.b) {
+                if (!this.character.isBubbling && !this.character.isBubblingPoison) {
+                    this.character.isBubbling = true;
+                    this.character.currentImage = 0;
                 }
-                if (this.keyboard.v) {
-                    if (!this.character.isBubbling && !this.character.isBubblingPoison) {
-                        if (this.character.collectedPoisons > 0) {
-                            this.character.isBubblingPoison = true;
-                        } else { this.character.isBubbling = true; }
-                        this.character.currentImage = 0;
-                    }
+            }
+            if (this.keyboard.v) {
+                if (!this.character.isBubbling && !this.character.isBubblingPoison) {
+                    if (this.character.collectedPoisons > 0) {
+                        this.character.isBubblingPoison = true;
+                    } else { this.character.isBubbling = true; }
+                    this.character.currentImage = 0;
                 }
+            }
 
             //barrier
-            this.level.backgroundObjects.filter(o=> o instanceof Barrier).forEach(barrier => {
+            this.level.backgroundObjects.filter(o => o instanceof Barrier).forEach(barrier => {
                 if (this.character.isColliding(barrier)) {
-                    this.character.barrierCollision = true;     
+                    this.character.barrierCollision = true;
                     this.character.calcDistanceToBarrierEnd(barrier);
                 }
             });
 
-                //collect items
-                this.level.collectableObjects.forEach((object) => {
-                    if (this.character.isColliding(object)) {
-                        this.character.collect(object);
-                        object.delete(this.level.collectableObjects, object);
+            //collect items
+            this.level.collectableObjects.forEach((object) => {
+                if (this.character.isColliding(object)) {
+                    this.character.collect(object);
+                    object.delete(this.level.collectableObjects, object);
+                }
+            });
+
+            //endboss
+            if (this.character.collisionMaxX >= this.endboss.x - 250) {
+                this.endboss.startAnimation(); //endboss is introduced                  
+            }
+            this.endboss.calculateCollisionCoordinates();
+            if (this.character.collisionMaxX > this.endboss.collisionMinX - 70 && !this.endboss.attack) {
+                this.endboss.currentImage = 0;
+                this.endboss.isAttacking(this.character);
+                if (soundOn) { this.endboss.attack_sound.play(); }
+            }
+
+            //bubbles
+            this.bubbles.forEach(bubble => {
+                bubble.calculateCollisionCoordinates();
+                if (bubble.collisionMaxY < 0) {
+                    bubble.delete(this.bubbles, bubble);
+                } else if (this.endboss.isColliding(bubble)) {//bubble meets endboss
+                    if (bubble.type == 'poisoned') {
+                        this.endboss.hit();
+                        if (soundOn) { this.character.bubble_sound.play(); }
                     }
-                });
-
-                //endboss
-                if (this.character.collisionMaxX >= this.endboss.x - 250) {
-                    this.endboss.startAnimation(); //endboss is introduced                  
-                }
-                this.endboss.calculateCollisionCoordinates();
-                if (this.character.collisionMaxX > this.endboss.collisionMinX - 70 && !this.endboss.attack) {
-                    this.endboss.currentImage = 0;
-                    this.endboss.isAttacking(this.character);
-                    if (soundOn) { this.endboss.attack_sound.play(); }
-                }
-
-                //bubbles
-                this.bubbles.forEach(bubble => {
-                    bubble.calculateCollisionCoordinates();
-                    if (bubble.collisionMaxY < 0) {
-                        bubble.delete(this.bubbles, bubble);
-                    } else if (this.endboss.isColliding(bubble)) {//bubble meets endboss
-                        if (bubble.type=='poisoned') {
-                            this.endboss.hit();
+                    bubble.delete(this.bubbles, bubble);
+                } else {
+                    this.level.enemies.forEach(enemy => {
+                        if (bubble.isColliding(enemy)) {//bubble meets enemy
+                            if (enemy instanceof Pufferfish) {
+                                bubble.delete(this.bubbles, bubble);
+                            } else {//bubble meets jellyfish
+                                bubble.withJelly(enemy.color);
+                                enemy.delete(this.level.enemies, enemy);
+                            }
                             if (soundOn) { this.character.bubble_sound.play(); }
                         }
-                        bubble.delete(this.bubbles, bubble);
-                    } else {
-                        this.level.enemies.forEach(enemy => {
-                            if (bubble.isColliding(enemy)) {//bubble meets enemy
-                                if (enemy instanceof Pufferfish) {
-                                    bubble.delete(this.bubbles, bubble);
-                                } else {//bubble meets jellyfish
-                                    bubble.withJelly(enemy.color);
-                                    enemy.delete(this.level.enemies, enemy);
-                                }
-                                if (soundOn) { this.character.bubble_sound.play(); }
-                            }
-                        });
-                    }
+                    });
+                }
 
-                });
+            });
         }, 1000 / 60);
     }
 
 
-
+    /**
+     * main function for drawing objects
+     */
     draw() {
         //clear canvas completely to enable redrawing
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -202,32 +220,43 @@ class World {
         this.addStaticToCanvas(this.endboss.lifeBarEndboss);
 
         let self = this;
-        this.drawRepeat=requestAnimationFrame(function () {
+        this.drawRepeat = requestAnimationFrame(function () {
             self.draw();
         });
     }
 
-
+    /**
+     * draws all (movable) objects of an array
+     * @param {array} objects - array of movable objects
+     */
     addObjectsToCanvas(objects) {
         objects.forEach(o => {
             this.addToCanvas(o);
         })
     }
 
-
+    /**
+     * draws a single movable object
+     * flips the image if object doesn't move in its standard direction
+     * @param {object} mo - movable object
+     */
     addToCanvas(mo) {
         if (mo.otherDirection) {
             mo.flipImage(ctx);
         }
         mo.draw(this.ctx);
         // mo.drawFrame(this.ctx);
-
         if (mo.otherDirection) {
             mo.flipImageBack(ctx);
         }
     }
 
 
+    /**
+     * draws all objects of an array
+     * these objects don't have a direction
+     * @param {array} objects - array of objects
+     */
     addStaticObjectsToCanvas(objects) {
         objects.forEach(o => {
             this.addStaticToCanvas(o);
@@ -235,6 +264,10 @@ class World {
     }
 
 
+    /**
+     * draws a single object without direction
+     * @param {object} o -  object
+     */
     addStaticToCanvas(o) {
         o.draw(this.ctx);
     }
